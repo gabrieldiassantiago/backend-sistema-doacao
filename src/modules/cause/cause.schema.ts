fc8a145c116e00a0de60288a72ae3b1db3cf7b60
Gabriel Dias Santiago <gabrieldiassantiago@hotmail.com>
2026-03-22 22:25:33 -0300

    refactor: update cause schemas to use Numeric and Files types for goalAmount and images

--- src/modules/cause/cause.schema.ts
+++ src/modules/cause/cause.schema.ts
@@ -3,18 +3,18 @@ import { t } from "elysia";
 export const CreateCauseSchema = t.Object({
     title: t.String({ minLength: 1 }),
     description: t.String({ minLength: 1 }),
-    goalAmount: t.Number({ minimum: 1 }),
-    imageUrl: t.Optional(t.String({ minLength: 1 })),
-    isFeatured: t.Optional(t.Boolean())
+    goalAmount: t.Numeric({ minimum: 1 }),
+    images: t.Files({ minItems: 1 }),
+    isFeatured: t.Optional(t.BooleanString())
 })
 
 export const UpdateCauseSchema = t.Object({
-    title: t.Optional(t.String({ minLength: 1 })    ),
+    title: t.Optional(t.String({ minLength: 1 })),
     description: t.Optional(t.String({ minLength: 1 })),
-    goalAmount: t.Optional(t.Number({ minimum: 1 })),
-    imageUrl: t.Optional(t.String({ minLength: 1 })),
-    isFeatured: t.Optional(t.Boolean())
-})
+    goalAmount: t.Optional(t.Numeric({ minimum: 1 })),
+    images: t.Optional(t.Files()),
+    isFeatured: t.Optional(t.BooleanString())
+})  
 
 export const CauseParamsSchema = t.Object({
     id: t.String()
